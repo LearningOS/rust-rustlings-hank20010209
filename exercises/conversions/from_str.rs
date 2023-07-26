7d33a1567364b3788cf153fe353e432ae4faae32
Hank <hank20010209@gmail.com>
2023-07-26 13:28:05 +0800

    Finish: 94/94, all complete

--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -28,7 +28,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -46,6 +45,23 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            return Err(ParsePersonError::Empty);
+        } else if s.matches(",").count() != 1{
+            return Err(ParsePersonError::BadLen);
+        } else {
+            let string_vec: Vec<&str> = s.split(',').collect();
+            let age_obj = match string_vec[1].parse::<usize>() {
+                Err(e) => {return Err(ParsePersonError::ParseInt(e));},
+                Ok(msg) => msg,
+            };
+            let name_obj = match string_vec[0].len() == 0 {
+                true => {return Err(ParsePersonError::NoName);},
+                false => string_vec[0].to_string()
+            };
+
+            return Ok(Person{name: name_obj, age: age_obj});
+        }
     }
 }
 
--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -23,7 +23,6 @@ enum IntoColorError {
     IntConversion,
 }
 
-// I AM NOT DONE
 
 // Your task is to complete this implementation
 // and return an Ok result of inner type Color.
@@ -38,6 +37,11 @@ enum IntoColorError {
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        let check = |x: i16| -> bool {!(x >= 0 && x <= 255)};
+        if check(tuple.0) == true || check(tuple.1) == true || check(tuple.2) == true {
+            return Err(IntoColorError::IntConversion);
+        }
+        Ok(Color{red: tuple.0 as u8, green: tuple.1 as u8, blue: tuple.2 as u8})
     }
 }
 
@@ -45,6 +49,13 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        let check = |x: i16| -> bool {!(x >= 0 && x <= 255)};
+        for i in arr {
+            if check(i) == true{
+                return Err(IntoColorError::IntConversion);
+            }
+        }
+        Ok(Color{red: arr[0] as u8, green: arr[1] as u8, blue: arr[2] as u8})
     }
 }
 
@@ -52,6 +63,16 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            return Err(IntoColorError::BadLen);
+        }
+        let check = |x: i16| -> bool {!(x >= 0 && x <= 255)};
+        for i in slice {
+            if check(*i) == true {
+                return Err(IntoColorError::IntConversion);
+            }
+        }
+        Ok(Color{red: slice[0] as u8, green: slice[1] as u8, blue: slice[2] as u8})
     }
 }
 
